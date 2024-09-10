dab02a6a059ebde3d5f7d161d6a6603c264fe1c4
vaibhav kumawat <vaibhavkumawat@itgeeks.com>
2024-09-10 14:56:56 +0530

    added affiliate code

--- server/app/v1/routes/affiliate.routes.js
+++ server/app/v1/routes/affiliate.routes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const controllers = require("../controllers/affiliate.controller.js");
 const router = express.Router();
 // const validation = require("../validations/auth.validation.js");
+const {authenticate} = require('../middleware/authentication.js')
 
-router.post('/add', controllers.addAffiliate);
-router.get('/:id', controllers.redirectShortLink);
+router.post('/add', authenticate,controllers.addAffiliate);
+router.post('/list',controllers.getAffiliate)
+router.post('/:id', controllers.redirectShortLink);
+
+
+
+//list affiliate 
+// password update 
 
 
 module.exports = router;
