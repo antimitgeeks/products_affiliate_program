c891658b8f4cf53363b30ffd44afbbf520183948
ankit-rathod-itGeeks <ankitrathod@itGeeks.com>
2024-09-11 13:25:49 +0530

    no changes

--- server/app/v1/routes/affiliate.routes.js
+++ server/app/v1/routes/affiliate.routes.js
@@ -3,7 +3,6 @@ const controllers = require("../controllers/affiliate.controller.js");
 const router = express.Router();
 const validation = require("../validations/affiliate.validation.js");
 
-
 const {authenticate} = require('../middleware/authentication.js')
 
 router.post('/add', authenticate,controllers.addAffiliate);
