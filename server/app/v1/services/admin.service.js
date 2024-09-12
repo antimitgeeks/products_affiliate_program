2b7c28d6bd87faefc4fa96ba432c8451e9f5182a
ankit-rathod-itGeeks <ankitrathod@itGeeks.com>
2024-09-12 11:58:47 +0530

    removed logs and added admin all users functionality

--- server/app/v1/services/admin.service.js
+++ server/app/v1/services/admin.service.js
@@ -1,21 +1,17 @@
 const shortid = require('shortid');
 const db = require("../models");
 const Affiliate = db.affiliate;
+const Users=db.users
+// const Users
 const jwt = require('jsonwebtoken');
 const fs = require('fs')
 const path=require('path')
 
-exports.allUsers = async (req, res, shortId) => {
+exports.allUsers = async () => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
-        const userId = jwt.decode(token).id
-        console.log(req.file);
-        const details = { ...req.body, userId, url: req.file.originalname }
-        details.shortId = shortId
-        const host = await req.headers.host
-        details.shortUrl = `${host}${process.env.BASE_URL}/affiliate/${shortId}`
-
-        const result = await Affiliate.create(details)
+        const result = await Users.findAll({
+            attributes:["email","country","city","address","companyName","companyNumber"]
+        })
         if (result) {
             return {
                 status: true,
