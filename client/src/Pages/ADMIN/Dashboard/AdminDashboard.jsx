d4912b783f6d0bff42f197d5938bea56a529eba3
Antim_Fulwere <antimfulwere@itgeeks.com>
2024-09-16 19:06:45 +0530

    added new freature

--- client/src/Pages/ADMIN/Dashboard/AdminDashboard.jsx
+++ client/src/Pages/ADMIN/Dashboard/AdminDashboard.jsx
@@ -7,48 +7,6 @@ import { MdRemoveRedEye } from "react-icons/md";
 import { FaSquarePlus } from "react-icons/fa6";
 
 function AdminDashboard({ loading, ListData }) {
-  // const invoices = [
-  //   {
-  //     id: 1,
-  //     themeName: "Elegant Portfolio",
-  //     domain: "elegantportfolio.com",
-  //     date: "2024-09-01",
-  //     commission: "$200",
-  //     status: "Paid",
-  //   },
-  //   {
-  //     id: 2,
-  //     themeName: "Modern Blog",
-  //     domain: "modernblog.net",
-  //     date: "2024-08-15",
-  //     commission: "$150",
-  //     status: "Pending",
-  //   },
-  //   {
-  //     id: 3,
-  //     themeName: "Tech Startup",
-  //     domain: "techstartup.io",
-  //     date: "2024-07-30",
-  //     commission: "$300",
-  //     status: "Paid",
-  //   },
-  //   {
-  //     id: 4,
-  //     themeName: "ItGeeks",
-  //     domain: "itgeeks.io",
-  //     date: "2024-08-30",
-  //     commission: "$100",
-  //     status: "Paid",
-  //   },
-  //   {
-  //     id: 5,
-  //     themeName: "Krown",
-  //     domain: "Krown.io",
-  //     date: "2024-08-30",
-  //     commission: "$120",
-  //     status: "Pending",
-  //   }
-  // ];
 
   const navigate = useNavigate();
 
--- server/app/v1/validations/schema/invoice.schema.js
+++ server/app/v1/validations/schema/invoice.schema.js
@@ -1,12 +1,13 @@
-const Joi=require('joi')
-exports.createInvoiceSchema=Joi.object({
-    themeName:Joi.string().required(),
-    userId:Joi.number().integer().required(),
-    domain:Joi.string().required(),
-    commission:Joi.number().integer().required()
+const Joi = require('joi')
+exports.createInvoiceSchema = Joi.object({
+    themeName: Joi.string().required(),
+    userId: Joi.number().integer().required(),
+    domain: Joi.string().optional(),
+    commission: Joi.number().integer().required(),
+    sourceId: Joi.string().required()
 })
 
 
-exports.updateStatuSchema=Joi.object({
-   status:Joi.string().required().valid("Paid","Pending","Failed")
+exports.updateStatuSchema = Joi.object({
+    status: Joi.string().required().valid("Paid", "Pending", "Failed")
 })
