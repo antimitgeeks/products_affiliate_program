a5399914e4726d464b78c97916af2e0126a1844f
ankit-rathod-itGeeks <ankitrathod@itGeeks.com>
2024-09-16 19:47:53 +0530

    changed limit in invoice pagination from 2 to 10

--- server/app/v1/services/invoice.service.js
+++ server/app/v1/services/invoice.service.js
@@ -24,7 +24,7 @@ exports.getInvoiceList = async (id, req) => {
 
     try {
         const page = parseInt(req.body.page) || 1;  // Default to page 1
-        const limit = parseInt(req.body.limit) || 2;  // Default to 10 items per page
+        const limit = parseInt(req.body.limit) || 10;  // Default to 10 items per page
         const offset = (page - 1) * limit;
 
         const result = await Invoice.findAll({
