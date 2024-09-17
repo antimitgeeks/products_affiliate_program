df9b7c4482b11f4d328bd7e4448c5480c5a2050f
Antim_Fulwere <antimfulwere@itgeeks.com>
2024-09-17 13:58:27 +0530

    added new code

--- client/src/Pages/Analytics/AnalyticsWrapper.jsx
+++ client/src/Pages/Analytics/AnalyticsWrapper.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Analytics from './Analytics';
+import { useGetAnalyticsDetailsQuery } from '../../services/AnalyticsService';
 
 function AnalyticsWrapper() {
+
+    const [loading, setLoading] = useState(false);
+    const [analyticsData, setAnalyticsData] = useState([]);
+
+    const { data, isLoading, isFetching } = useGetAnalyticsDetailsQuery({ Id: 4, data: { "type": "purchases" } })
+
+    useEffect(() => {
+        console.log(data, '-----------------------------------------analyticsDetail');
+
+    }, [data, isLoading, isFetching])
+
+    console.log(analyticsData, '-----------------------------analyticsDetail');
+
+
+    useEffect(() => {
+        if (isLoading || isFetching) {
+            setLoading(true);
+        }
+        else {
+            setLoading(false);
+            setAnalyticsData(data?.result)
+        }
+    }, [isLoading, isFetching])
+
     return (
         <>
             <div className='page-body px-4'>
-                <Analytics />
+                <Analytics loading={loading} analyticsData={analyticsData} />
             </div>
         </>
     )
