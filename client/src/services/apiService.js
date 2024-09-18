0aa3f9f9f3d17c4033f842647b086d3dffd9c39f
Antim_Fulwere <antimfulwere@itgeeks.com>
2024-09-18 11:30:45 +0530

    added new code

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 const CreateApi = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://48f9-2401-4900-1ca2-1a1d-acf8-7676-4504-e8f4.ngrok-free.app/api/v1",
+        baseUrl: "http://localhost:3000/api/v1",
         prepareHeaders: (headers) => {
             const user = Cookies.get('isLogged');
             if (user) {
@@ -16,7 +16,7 @@ const CreateApi = createApi({
         mode: 'cors', // Ensure mode is set to 'cors'
     }),
     endpoints: () => ({}),
-    tagTypes: ["adminPanel", "links", "adminAffiliate"]
+    tagTypes: ["adminPanel", "links", "adminAffiliate", "assignCustomer"]
 });
 
 export default CreateApi;
