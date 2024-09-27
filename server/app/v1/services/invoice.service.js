2a002b1119b8a7981e148a7240fcb48b4287d755
vaibhav kumawat <vaibhavkumawat@itgeeks.com>
2024-09-27 11:09:24 +0530

    invoice search on basis of themeName ,domain ,transactionId

--- server/app/v1/services/invoice.service.js
+++ server/app/v1/services/invoice.service.js
@@ -40,6 +40,12 @@ exports.getInvoiceList = async (id, req) => {
                         [Op.like]: `${query}%`,
                         // [Op.like]: {shortId:`${query}%`}
                     },
+                    domain:{
+                        [Op.like]:`${query}`
+                    },
+                    transactionId:{
+                        [Op.like]:`${query}`
+                    }
                 }
             },
             order: [
